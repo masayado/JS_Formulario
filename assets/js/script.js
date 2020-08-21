22ad7a5d924dea16d411a8e56edbf7f7e1da1be9
masayado <masayado@uc.cl>
2020-08-20 20:20:22 -0400

    formulario completo

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+function validar(){
+    var nombre
+    nombre = document.getElementById("nombre").value;  
+
+    if (nombre === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (nombre.length>30){
+        alert("Tu nombre supera la cantidad de caracteres");
+        return false;
+    }
+
+    var apellidos
+    apellidos = document.getElementById("apellidos").value;  
+
+    if (apellidos === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (apellidos.length>80){
+        alert("Tus apellidos superan cantidad de caracteres");
+        return false;
+    }
+
+    var correo 
+    correo = document.getElementById("correo").value;
+
+    var arroba = correo.indexOf("@");
+    var punto = correo.lastIndexOf(".");
+
+    if (correo === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (arroba < 0){
+        alert("Ingrese correo válido");
+        return false;
+    }
+    else if (arroba+2 > punto){
+        alert("Ingrese correo válido");
+        return false;
+    }
+    else if (punto >= correo.length-1 ){
+        alert("Ingrese correo válido");
+        return false;
+    }
+    else if (correo.length>100){
+        alert("Tu correo supera cantidad de caracteres");
+        return false;
+    }
+
+    var usuario
+    usuario = document.getElementById("usuario").value;  
+
+    if (usuario === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (usuario.length>20){
+        alert("Tu nombre de usuario supera la cantidad de caracteres");
+        return false;
+    }
+
+    var pass
+    pass = document.getElementById("pass").value;  
+
+    if (usuario === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+
+    var telefono
+    var telefonoanumero
+
+    telefono = document.getElementById("telefono").value;  
+    telefonoanumero = Number(telefono);
+
+    if (telefono === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (telefono.length>15){
+        alert("Tu número de teléfono cantidad de caracteres");
+        return false;
+    }
+    else if(Number.isNaN(telefonoanumero)){
+        alert("Ingrese un número válido");
+        return false;
+    }
+
+    saludo = 'Bienvenid@ ' + nombre + ' ' + apellidos + '.' + '\nTu nombre de usuario es ' + usuario + ' y tu password es ' + pass + '.'
+
+    alert(saludo);
+}
